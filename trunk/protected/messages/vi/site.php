c123837f01f14e5dc802eda7c32b7b314d668764
tonyle.microsoft@gmail.com <tonyle.microsoft@gmail.com@b00b507b-91a5-3b86-3d45-e291d1989001>
2014-08-02 15:26:46 +0000

    delete all source

--- trunk/protected/messages/vi/site.php
+++
@@ -1,4 +0,0 @@
-<?php 
-return array (
-  'username' => 'Tên đăng nhập',
-);
--- trunk/protected/views/site/_right.php
+++
@@ -1,9 +0,0 @@
-<?php $this->beginClip('right');  $this->renderPartial("//site/_calendar"); ?>
-
-<!-- Begin banner đổi số trúng -->
-<div class="panel panel-danger"><div class="panel-body" style="padding: 0;">
-<img src="<?php echo Yii::app()->homeUrl ?>img/banner_doi_so_trung.jpg" class="img-responsive" />
-</div></div>
-<!-- End banner đổi số trúng -->
-
-<?php $this->endClip(); ?>
